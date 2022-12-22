51fa29fadf71f2d9f76ab427fa16cae977c6401f
Barbara Hellen <barbara.pereira@modalgr.com.br>
2022-12-22 15:36:14 -0300

    feat: cria a header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -11,7 +10,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>hello word</h1>
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      {/* Footer */}
     </>
   );
 }
