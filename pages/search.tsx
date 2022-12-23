c57d29bf9dd89e12124c5e5ad755dea984503a9d
Barbara Hellen <barbara.pereira@modalgr.com.br>
2022-12-22 22:56:07 -0300

    feat: pega dados de pesquisa api google

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import React from "react";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../Response";
 
-export default function search() {
+export default function search({ results }: any) {
+  console.log("results", results);
   return (
     <div>
       <Head>
@@ -13,4 +15,23 @@ export default function search() {
       {/* search Result */}
     </div>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const mockData = true;
+  const data = mockData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
